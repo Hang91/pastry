af5b8f51451df3dcfe927dee9199b5fa192535fe
Hang Jin <hangjin@Hangs-MacBook-Pro.local>
2017-10-21 00:19:02 -0400

    binary search nodeIndex, leaf set finished. Doing route table building part

--- lib/pastry/pastry_node.ex
+++ lib/pastry/pastry_node.ex
@@ -1,4 +1,20 @@
 defmodule PASTRY.Pastry_node do
-	def start_node(nodeId, numRequests) do
+	
+	use GenServer
+
+	def start_node(nodeId, numNodes, numRequests) do
+		nodeIdStr = Integer.to_string(nodeId)
+		hashId = get_hash(nodeIdStr)
+		GenServer.start_link(__MODULE__, [], [name: String.to_atom(hashId)])
 	end
+
+	def handle_cast(msg, []) do
+		{:noreply, []}
+	end
+
+	def get_hash(input) do
+		:crypto.hash(:md5, input)
+		|> Base.encode16
+	end
+
 end
--- lib/pastry/pastry_system.ex
+++ lib/pastry/pastry_system.ex
@@ -1,6 +1,10 @@
 defmodule PASTRY.Pastry_system do
 	def start_system(numNodes, numRequests) do
 		IO.puts "start system with #{numNodes} and #{numRequests}"
-		
+		nodesNum = String.to_integer(numNodes)
+		for n <- 1..nodesNum do
+			PASTRY.Pastry_node.start_node(n, nodesNum, numRequests)
+		end
 	end
+
 end
